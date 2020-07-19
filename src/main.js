8fa7b4e29482fe8447370673df4946e35236d082
Naziks <naziks.media@gmail.com>
2020-07-19 03:29:24 +0300

    Init

--- src/main.js
+++ src/main.js
@@ -4,21 +4,47 @@ class loader{
 		this.modules = modules;
 	}
 
-	load(){
+	load(id=-1, result=[]){
+		let self = this;
 
+		if(this.modules.length == 0) return console.log('[nLoader] No modules found.');
+		id += 1;
+		if(id > this.modules.length-1){
+			return console.log('[nLoader] Result: ' ,result)
+		}
+		this.request(this.modules[id], r => {
+			result[this.modules[id]] = r;
+			self.load(id, result)
+		})
 	}
 
-	request(url, callback = ()=>{}){
+	addToPage(module, content){
+		try{
+			eval(content);
+			return true;
+		}catch(e){
+			console.error(`[nLoader] Module error (${module}): `, e);
+			return false;
+		}
+	}
+
+	request(module, callback = ()=>{}){
+		let self = this;
 		let a = new XMLHttpRequest();
 		a.onreadystatechange = function(e){
-			console.log(a)
-			console.log(a.readyState)
+			if(a.readyState == 4){
+				if(a.status == 200){
+					callback(self.addToPage(module, a.responseText));
+				}else{
+					console.log('[nLoader] Module not found `'+module+'`')
+				}
+			}
 		}
-		a.onerror = a.onabort = a.ontimeout = a.onloadend = function(e){
-			console.log(e)
+		a.onerror = a.onabort = a.ontimeout = function(e){
+			console.log('[nLoader] Error: ', e)
 		}
-		a.open('GET', url, false);
+		a.open('GET', `${this.path}/${module}`, false);
 		a.send();
-
+		return a;
 	}
 }
